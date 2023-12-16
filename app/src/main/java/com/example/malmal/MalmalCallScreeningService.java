3744ca7de6f4291d675fd098250ac76c7eaeaff8
yddook <hsn07103@kaist.ac.kr>
2023-12-16 14:47:31 +0900

    call screening works well for outgoing and incomming'

--- app/src/main/java/com/example/malmal/MalmalCallScreeningService.java
+++ app/src/main/java/com/example/malmal/MalmalCallScreeningService.java
@@ -1,42 +1,53 @@
-//package com.example.malmal;
-//
-//import android.database.Cursor;
-//import android.net.Uri;
-//import android.os.Build;
-//import android.provider.ContactsContract;
-//import android.telecom.Call;
-//import android.telecom.CallScreeningService;
-//import android.telecom.Connection;
-//import android.telephony.PhoneNumberUtils;
-//import android.util.Log;
-//
-//import java.util.Locale;
-//
-//public class MalmalCallScreeningService extends CallScreeningService {
-//    @Override
-//    public void onScreenCall(Call.Details details) {
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-//            if (details.getCallDirection() == Call.Details.DIRECTION_INCOMING) {
-//                switch (details.getCallerNumberVerificationStatus()) {
-//                    case Connection.VERIFICATION_STATUS_NOT_VERIFIED:
-////                        Timber.w("No caller verification was performed for " + details.getFormattedPhoneNumber() + "!");
-//                        Log.d("");
-//                        break;
-//                    case Connection.VERIFICATION_STATUS_FAILED:
-////                        Timber.e("Caller " + details.getFormattedPhoneNumber() + " FAILED verification!");
-//                        Log.d("");
-//                        break;
-//                    case Connection.VERIFICATION_STATUS_PASSED:
-////                        Timber.i("Caller " + details.getFormattedPhoneNumber() + " is verified...");
-//                        Log.d("");
-//                        break;
-//                }
-//
-//                String caller = null;
-//                if (details.getHandle() != null) {
-//                    caller = getContactName(details.getHandle().getSchemeSpecificPart());
-//                }
-//
+package com.example.malmal;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.telecom.Call;
+import android.telecom.CallScreeningService;
+import android.telecom.Connection;
+import android.telephony.PhoneNumberUtils;
+import android.util.Log;
+
+import java.util.Locale;
+
+public class MalmalCallScreeningService extends CallScreeningService {
+    @Override
+    public void onScreenCall(Call.Details callDetails) {
+        Log.d("MalmalCallScreeningService", "ENTERED");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            boolean isIncoming = callDetails.getCallDirection() == Call.Details.DIRECTION_INCOMING;
+            boolean isOutgoing = callDetails.getCallDirection() == Call.Details.DIRECTION_OUTGOING;
+
+            if (isIncoming) {
+                switch (callDetails.getCallerNumberVerificationStatus()) {
+                    case Connection.VERIFICATION_STATUS_NOT_VERIFIED:
+//                        Timber.w("No caller verification was performed for " + details.getFormattedPhoneNumber() + "!");
+                        Log.d("MalmalCallScreeningService", "VERIFICATION_STATUS_NOT_VERIFIED");
+                        break;
+                    case Connection.VERIFICATION_STATUS_FAILED:
+//                        Timber.e("Caller " + details.getFormattedPhoneNumber() + " FAILED verification!");
+                        Log.d("MalmalCallScreeningService", "VERIFICATION_STATUS_FAILED");
+                        break;
+                    case Connection.VERIFICATION_STATUS_PASSED:
+//                        Timber.i("Caller " + details.getFormattedPhoneNumber() + " is verified...");
+                        Log.d("MalmalCallScreeningService", "VERIFICATION_STATUS_PASSED");
+                        break;
+                }
+
+                Uri handle = callDetails.getHandle();
+                String phoneNumber = (handle != null) ? handle.getSchemeSpecificPart() : null;
+
+                if (phoneNumber != null) {
+                    Log.d("INCOMMING PHONE NUMBER", ""+phoneNumber);
+                }
+
+                String caller = null;
+                if (callDetails.getHandle() != null) {
+                    caller = getContactName(callDetails.getHandle().getSchemeSpecificPart());
+                }
+
 //                boolean rejectDueToVerificationStatus = false;
 //                switch (details.getCallerNumberVerificationStatus()) {
 //                    case Connection.VERIFICATION_STATUS_FAILED:
@@ -46,25 +57,32 @@
 ////                        rejectDueToVerificationStatus = ScreeningPreferences.declineUnauthenticatedCallers();
 //                        break;
 //                }
-//
-////                boolean rejectDueToUnknownCaller = caller == null && ScreeningPreferences.declineUnknownCallers();
-//
+
+//                boolean rejectDueToUnknownCaller = caller == null && ScreeningPreferences.declineUnknownCallers();
+
 //                CallResponse response;
 //                if (rejectDueToVerificationStatus || rejectDueToUnknownCaller) {
 //                    response = buildRejectionResponse();
 //                } else {
 //                    response = buildAcceptResponse();
 //                }
-//
+
 //                respondToCall(details, response);
-//            }
-//        }
-//    }
-//
-//    private CallScreeningService.CallResponse buildAcceptResponse() {
-//        return new CallScreeningService.CallResponse.Builder().build();
-//    }
-//
+            } else if (isOutgoing) {
+                Uri handle = callDetails.getHandle();
+                String phoneNumber = (handle != null) ? handle.getSchemeSpecificPart() : null;
+
+                if (phoneNumber != null) {
+                    Log.d("OUTGOING PHONE NUMBER", ""+phoneNumber);
+                }
+            }
+        }
+    }
+
+    private CallScreeningService.CallResponse buildAcceptResponse() {
+        return new CallScreeningService.CallResponse.Builder().build();
+    }
+
 //    private CallScreeningService.CallResponse buildRejectionResponse() {
 //        Timber.d("Reject? " + ScreeningPreferences.isServiceEnabled()
 //                + ", Notify? " + !ScreeningPreferences.skipCallNotification()
@@ -76,31 +94,31 @@
 //                .setSkipCallLog(ScreeningPreferences.skipCallLog())
 //                .build();
 //    }
-//
-//    private String getContactName(String phoneNumber) {
-//        Uri uri = Uri.withAppendedPath(
-//                ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
-//                Uri.encode(phoneNumber)
-//        );
-//
-//        String[] projection = new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME};
-//        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-//        if (cursor != null) {
-//            try {
-//                if (cursor.moveToFirst()) {
-//                    return cursor.getString(0);
-//                }
-//            } finally {
-//                cursor.close();
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private static class CallDetailsExtensions {
-//        static String getFormattedPhoneNumber(Call.Details details) {
-//            String phoneNumber = details.getHandle() != null ? details.getHandle().getSchemeSpecificPart() : null;
-//            return phoneNumber != null ? PhoneNumberUtils.formatNumber(phoneNumber, Locale.getDefault().getCountry()) : "BLOCKED";
-//        }
-//    }
-//}
+
+    private String getContactName(String phoneNumber) {
+        Uri uri = Uri.withAppendedPath(
+                ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                Uri.encode(phoneNumber)
+        );
+
+        String[] projection = new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME};
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            try {
+                if (cursor.moveToFirst()) {
+                    return cursor.getString(0);
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        return null;
+    }
+
+    private static class CallDetailsExtensions {
+        static String getFormattedPhoneNumber(Call.Details details) {
+            String phoneNumber = details.getHandle() != null ? details.getHandle().getSchemeSpecificPart() : null;
+            return phoneNumber != null ? PhoneNumberUtils.formatNumber(phoneNumber, Locale.getDefault().getCountry()) : "BLOCKED";
+        }
+    }
+}
